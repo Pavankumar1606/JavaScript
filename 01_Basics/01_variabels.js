c7d2176aa8af805d386a305510da421d8fd661b0
Pavankumar1606 <128731078+Pavankumar1606@users.noreply.github.com>
2024-05-02 05:32:08 +0000

    Variables

---
+++ 01_Basics/01_variabels.js
@@ -0,0 +1,14 @@
+const accountId=12345;
+let accountEmail="Pavan@gmail.com";
+ var accountPassword="hyderabad";
+
+ let accountState;
+
+//  accountId=2;
+
+accountEmail="asdasdsd";
+accountPassword="2121212";
+accountCity="Bangalure";
+
+ console.log(accountId);
+ console.table([accountId, accountEmail, accountPassword, accountCity,accountState])
