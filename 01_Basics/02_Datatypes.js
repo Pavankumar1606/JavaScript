7036b5615efbbb747f8a6c228fe78b873b77d01e
Pavankumar1606 <128731078+Pavankumar1606@users.noreply.github.com>
2024-05-14 12:32:45 +0000

    datatypes

---
+++ 01_Basics/02_Datatypes.js
@@ -0,0 +1,26 @@
+"Use Strict"; // treat all JS code as newer version
+
+// alert(3+3);   // we are using nodejs, not browser
+
+console.log(3
+    +3)// code redability should be high
+
+console.log("Pavan")
+
+let name="pavan"
+let age=23
+let isLoggedIn=true
+
+//number==>2 to pow 53
+//bigInt
+//String==>" "
+//Boolean==> true/false
+//null==> standalone value
+//undefined==>
+//symbol 
+
+
+//object
+
+
+console.log(typeof null)
