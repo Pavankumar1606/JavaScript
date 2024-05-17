fc0182d2594dcf41066518746fc439fb44974c6c
Pavankumar1606 <128731078+Pavankumar1606@users.noreply.github.com>
2024-05-17 14:08:09 +0000

    Objects in js

---
+++ 02_Basics/04_objects.js
@@ -0,0 +1,21 @@
+// const tinderUser=new Object()
+const tinderUser={}
+
+tinderUser.id="123abc"
+tinderUser.name="pavan"
+tinderUser.isLoggedIn=false
+
+// console.log(tinderUser);
+
+const regularUser={
+    email:"pavan@gmail.com",
+    fullname:{
+        userfullname:{
+            firstname:"Pavan",
+            lastname:"kumar"
+        }
+    }
+}
+// console.log(regularUser.fullname.userfullname.firstname);
+
+c
