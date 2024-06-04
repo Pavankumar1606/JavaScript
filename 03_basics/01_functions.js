15450ebe32dc343c0885d7fbc5718564ff1f3f88
Pavankumar1606 <128731078+Pavankumar1606@users.noreply.github.com>
2024-06-04 13:57:20 +0000

    functions

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,36 @@
+
+
+function sayMyName(){
+    console.log("P");
+    console.log("a");
+    console.log("v");
+    console.log("a");
+    console.log("n");
+}
+// sayMyName
+
+// function addTwoNumbers(number1, number2){
+//    console.log(number1+number2);
+// }
+
+function addTwoNumbers(number1, number2){
+//   let result=number1+number2
+//   console.log("Pavan");
+
+return number1+number2
+}
+
+const result=addTwoNumbers(3,5)
+
+// console.log("Result:",result);
+
+function loginUserMessage(username="sam"){
+    if(!username){
+        console.log("Please enter username");
+        return
+    }
+    return `${username} just logged in`
+}
+
+// console.log(loginUserMessage("Pavan"))
+console.log(loginUserMessage("Pavan"))
