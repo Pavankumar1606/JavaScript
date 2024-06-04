5ce5cb996dc58591ad5d830b11e1e97c716c2894
Pavankumar1606 <128731078+Pavankumar1606@users.noreply.github.com>
2024-06-04 14:10:28 +0000

    functions with objects and Arrays

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -33,5 +33,33 @@ function loginUserMessage(username="sam"){
 }
 
 // console.log(loginUserMessage("Pavan"))
-console.log(loginUserMessage("Pavan"))
-console.log(loginUserMessage("Pavan"))
+// console.log(loginUserMessage("Pavan"))
+
+function calculateCartPrice(val1, val2, ...num1){
+    return num1
+}
+
+// console.log(calculateCartPrice(200, 400, 500,2000,3000))
+
+const user={
+    username:"Pavan",
+    price:199
+}
+
+function handleObject(anyobject){
+    console.log(`username is ${anyobject.username}  and price is ${anyobject.price}`);
+}
+// handleObject(user)
+handleObject({
+    username:"sam",
+    price:300
+})
+
+const myNewArray=[200, 400, 500, 600]
+
+function returnSecondValue(getAarray){
+    return getAarray[1]
+}
+
+// console.log(returnSecondValue(myNewArray));
+console.log(returnSecondValue([200,400, 500, 1000]));
