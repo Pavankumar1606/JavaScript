21d4d84d243d377bf9c812c35a687ef64ecd399c
Pavankumar1606 <128731078+Pavankumar1606@users.noreply.github.com>
2024-06-04 13:36:48 +0000

    Json Objects

--- 02_Basics/04_objects.js
+++ 02_Basics/04_objects.js
@@ -52,10 +52,36 @@ const users=[
 ]
 
 users[1].email
-console.log(tinderUser);
+// console.log(tinderUser);
 
-console.log(Object.keys(tinderUser));
-console.log(Object.values(tinderUser));
-console.log(Object.entries(tinderUser));
+// console.log(Object.keys(tinderUser));
+// console.log(Object.values(tinderUser));
+// console.log(Object.entries(tinderUser));
 
-console.log(tinderUser.hasOwnProperty('isLoggedIn'));
+// console.log(tinderUser.hasOwnProperty('isLoggedIn'));
+
+
+
+const course={
+    coursename:"Javascript ",
+    price:"999",
+    courseInstructor:"Pavan"
+}
+
+// course.courseInstructor
+
+const{courseInstructor:instructor}=course
+
+console.log(instructor);
+
+// {
+//     "name":"pavan",
+//     "coursename":"javascript",
+//     "price":"free"
+// }
+
+[
+    {},
+    {},
+    {},
+]
